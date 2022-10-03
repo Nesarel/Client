c52337f9dab9022f3a1c35f8740a09fe01a10688
Nesarel <nesarel.gd@gmail.com>
2022-10-03 16:23:16 +0200

    test with callback

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-// const api = "https://curious-fox-flannel-nightgown.cyclic.app"
 const api = "http://localhost:3000/1"
 
-let BDD = []
+let bdd = []
 
-// async function getAPI(url) {
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     BDD = data;
-//     console.log(res);
-//     console.log(data);
-//     console.log(BDD);
-//     return BDD;
-// }
+async function getAPI(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    bdd = data;
+    displayData(bdd);
+}
+
+function displayData(tabData){
+    console.log(tabData);
+    document.body.innerHTML = tabData[0].code
+}
 
 
-fetch(api)
-.then(res => res.json())
-.then(code => document.body.innerHTML = code[0].code)
+// fetch(api)
+// .then(res => res.json())
+// .then(code => document.body.innerHTML = code[0].code)
 
-// document.body.innerHTML = code[0].code
-
-// getAPI(api)
+getAPI(api)
